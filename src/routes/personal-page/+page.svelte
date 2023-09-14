e86e086aee83bc656fec12e971072e7211a5a87b
r20222 <101579892+r20222@users.noreply.github.com>
2023-09-14 15:32:37 +0200

    desktop versie

--- src/routes/personal-page/+page.svelte
+++ src/routes/personal-page/+page.svelte
@@ -2,24 +2,48 @@
     <a class="go-back-button" href="/" title="Go back">&#8592; Go back</a>
 </header>
 
-<picture class="image-personal-page">
-    <img src="" alt="" title="">
-</picture>
+<div class="img-name-center">
+    <section class="desktop-img-name-container">
+        <picture class="image-personal-page">
+            <img src="" alt="" title="">
+        </picture>
+        
+        <article class="name-squad">
+            <h1>Naam</h1>
+            <p class="squad">Squad nr</p>
+        </article>
+    </section>
+</div>
 
-<h1>Naam</h1>
-<p class="squad">Squad nr</p>
 
 <a class="visitekaart-link" href="/" title="" target="_blank">Visit profile card &#8594;</a>
 
 <p class="bio">Biography BLablablablalbalblablalblalbla blablalbalblalblalblalblalbaba.</p>
 
+
+<h2 class="mobile-h2">Naam's skills</h2>
 <section class="all-skills">
-    <h2>Naam's skills</h2>
+    <div class="desktop-skills-visiterkaart">
+        <h2>Naam's skills</h2>
+        <a class="desktop-visitekaart-link" href="/" title="" target="_blank">Visit profile card &#8594;</a>
+    </div>
     <!-- hier each loop maken voor elke skill 1 div.skill -->
-    <div class="skill"></div>
-    <div class="skill"></div>
-    <div class="skill"></div>
-    <div class="skill"></div>
+    <div class="skill">
+        <img class="skill-img" src="" alt="" title="" width="50" height="50">
+        <h3>Skill name</h3>
+    </div>
+    <div class="skill">
+        <img class="skill-img" src="" alt="" title="" width="50" height="50">
+        <h3>Skill name</h3>
+    </div>
+    <div class="skill">
+        <img class="skill-img" src="" alt="" title="" width="50" height="50">
+        <h3>Skill name</h3>
+    </div>
+    <div class="skill">
+        <img class="skill-img" src="" alt="" title="" width="50" height="50">
+        <h3>Skill name</h3>
+    </div>
 </section>
 
 
@@ -28,6 +52,7 @@
     :global(html){
         background-color: #1F1F1F;
         color:white;
+        overflow-x: hidden;
     }
     :global(body){
         margin:0rem;
@@ -74,6 +99,7 @@
     }
     .bio{
         margin: 4rem 3.1rem 2rem 3.1rem;
+        max-width: 20rem;
     }
     .all-skills{
         width:100vw;
@@ -84,15 +110,114 @@
         gap:1rem;
         margin-bottom: 3rem;
     }
-    h2{
+    .mobile-h2{
         align-self: start;
         margin-left: 3.1rem;
-        margin-bottom:0;
+        margin-bottom:1rem;
     }
     .skill{
         height:64px;
         width: calc(100vw - 2.2rem);
         background-color: black;
         border-radius: 1rem;
+        display:flex;
+        align-items: center;
+        gap: 1rem;
+    }
+    .skill-img{
+        margin-left: 2rem;
+    }
+
+    .desktop-skills-visiterkaart{
+        display: none;
+    }
+
+    /* desktop vanaf 1000px */
+
+    @media screen and (min-width: 62.5em){
+        picture{
+            height: 234px;
+            width:20rem;
+        }
+    
+        picture img{
+            width:10rem;
+            height:10rem;
+            border-radius: 50%;
+            margin-top:2rem;
+            /* margin-left: 12rem; */
+        }
+        .img-name-center{
+            display: flex;
+            justify-content: center;
+        }
+        .desktop-img-name-container{
+            display:flex;
+            width: 80vw;
+        }
+        .name-squad{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+        h1{
+            margin-top: 0;
+            margin-left:0;
+        }
+        .bio{
+            /* margin-left:5.1rem; */
+            display: none;
+        }
+        .squad{
+            margin-left:0;
+            margin-bottom:0;
+            margin-top:0;
+        }
+        .visitekaart-link{
+            display: none;
+        }
+        .desktop-skills-visiterkaart{   
+            align-self: center;
+            margin-top: 5rem;
+            margin-bottom:0;
+            flex-direction: row;
+            display: flex;
+            width: 80vw; 
+            justify-content: space-between;
+        }
+        .desktop-visitekaart-link{
+            display:flex;
+            align-items: center;
+            margin-top:0;
+            margin-left:0;
+            margin-right: -2rem;
+            color:white;
+            background-color: black;
+            padding: .8rem 2rem;
+            font-size: 1.2rem;
+            border-radius: 50px;
+        }
+        .mobile-h2{
+            display: none;
+        }
+        h2{
+            margin-top:0.8rem;
+            padding-left:0;
+            /* margin-left:2rem; */
+        }
+        .skill-img{
+            margin-left: 2.5rem;
+        }
+        .skill{
+
+            width: 80vw;
+            height: 10rem;
+
+            border-radius:0;
+            border-bottom: 1px solid white;
+            background-color: transparent;
+            gap: 8rem;
+        }
+        
     }
 </style>
