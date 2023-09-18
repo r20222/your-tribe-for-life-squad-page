02e20cc97b65d77003a977ff20f82bd14af3eceb
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-09-18 17:04:56 +0200

    background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,7 @@
 <main>
-	<h1>hoi</h1>
-
 	<style>
 		body {
 			background: url(/bg-home.jpg);
-			background-size: cover;
-			background-repeat: no-repeat;
-			background-position: center;
 		}
 	</style>
 </main>
