543367a40eb80d42ad2a3fb8fd4a8186e2f0b74f
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-09-18 20:50:46 +0200

    server werkt niet!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
+<!-- <script>
+	import * as prismic from '@prismicio/client';
+	import { SliceZone } from '@prismicio/svelte';
+	import { components } from '$lib/slices';
+
+	export let data;
+
+	import Header from '$lib/components/Headerhome.svelte';
+</script> -->
+
+<!-- <SliceZone slices={data.slices} {components} /> -->
+
 <main>
 	<style>
 		body {
