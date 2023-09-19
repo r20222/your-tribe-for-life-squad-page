857c5a14a0aa783c7d266529a1b400e860d2a360
Amberhva <amber.van.der.reijden@hva.nl>
2023-09-19 16:30:21 +0200

    tes
    gsg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
-<!-- <script>
+<script>
 	import * as prismic from '@prismicio/client';
 	import { SliceZone } from '@prismicio/svelte';
 	import { components } from '$lib/slices';
 
 	export let data;
 
-	import Header from '$lib/components/Headerhome.svelte';
-</script> -->
+	// import Header from '$lib/components/Headerhome.svelte';
+</script>
 
-<!-- <SliceZone slices={data.slices} {components} /> -->
+<SliceZone slices={data.slices} {components} />
 
 <main>
 	<style>
