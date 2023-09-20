a2c3f39c498e40f2621375ca243a37e297378158
zenitba <112856019+zenitba@users.noreply.github.com>
2023-09-20 15:40:33 +0200

    home/login af

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,23 @@
 
+<main>
+    <slot />
+</main>
 
-<slot />
+<style>
+
+    :global(*) {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    main {
+    margin: auto;
+    overflow: auto;
+    background: linear-gradient(315deg, rgb(3, 5, 53) 3%, rgb(1, 32, 64) 38%, rgb(16, 13, 39) 68%, rgb(5, 10, 51) 98%);
+    animation: gradient 15s ease infinite;
+    background-attachment: fixed;
+    width: 100VW;
+    height: 100VH;
+}
+
+</style>
