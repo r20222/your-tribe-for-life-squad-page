c7885f7ea7283a2ff675ff107352f66ab2ee5898
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-09-21 13:04:09 +0200

    clean code

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -9,21 +9,6 @@
 
 <Header />
 
-<!-- <section class="squad">
-	{#each data.slices[0].items as item}
-		<article class="profile">
-			<img
-				src={item.squad_b.data.picture.url}
-				alt="Profile picture of Squad B"
-				width="100"
-				height="100"
-			/>
-
-			<h1 class="name">{item.squad_b.data.name}</h1>
-		</article>
-	{/each}
-</section> -->
-
 <section class="squad">
 	{#each data.slices[0].items as item}
 		<a href="/gallery/{item.squad_b.uid}">
@@ -42,10 +27,6 @@
 		</a>
 	{/each}
 </section>
-
-<!-- <div>
-	<pre>{JSON.stringify(data, null, 2)}</pre>
-</div> -->
 
 <style>
 	.squad {
